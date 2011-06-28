44342a5d035985062659cb414d8b467cf5e2044b
Guido van Rossum <guido@google.com>
2011-06-28 16:12:06 -0700

    Increase Appstats stack limit.

--- appengine_config.py
+++ appengine_config.py
@@ -9,6 +9,7 @@ def webapp_add_wsgi_middleware(app):
 
 appstats_KEY_DISTANCE = 10
 appstats_MAX_REPR = 1000
+appstats_MAX_STACK = 20
 
 appstats_FILTER_LIST = [
   {'PATH_INFO': '!^/favicon\.ico$'},
