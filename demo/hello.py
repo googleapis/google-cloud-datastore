7a6f6c0d59ab134ec06da147a7833927eac2fc80
Guido van Rossum <guido@google.com>
2011-07-07 21:06:47 -0700

    The USER_ID issue is fixed.

--- demo/hello.py
+++ demo/hello.py
@@ -9,14 +9,8 @@ class Greeting(model.Model):
     message = model.StringProperty()
     userid = model.IntegerProperty()  # Not used here, but later
 
-def fix_user_id():
-    email = os.getenv('USER_EMAIL')
-    if email and not os.getenv('USER_ID'):
-        os.environ['USER_ID'] = email
-
 class HomePage(webapp.RequestHandler):
     def get(self):
-        fix_user_id()
         msg = Greeting.get_or_insert('hello', message='Hello world')
         self.response.out.write(msg.message)
 
