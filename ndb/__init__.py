be85fa88e90ea6a7f4b6de9028cf5ea783a586a6
Guido van Rossum <guido@google.com>
2011-12-02 09:12:02 -0800

    Support from ndb import *. Fixes issue 102.

--- ndb/__init__.py
+++ ndb/__init__.py
@@ -1,3 +1,19 @@
 """NDB -- A new datastore API for the Google App Engine Python runtime."""
 
-__version__ = '0.9.3'
+__version__ = '0.9.3+'
+
+__all__ = []
+
+# TODO: Anything from eventloop or utils?  (They'd go here.)
+
+from .tasklets import *
+__all__ += tasklets.__all__
+
+from .model import *  # This implies key.*
+__all__ += model.__all__
+
+from .query import *
+__all__ += query.__all__
+
+from .context import *
+__all__ += context.__all__
