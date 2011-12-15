04a9eb1b9bd787a1b78fc0c2654aec4c2de9a3a6
Guido van Rossum <guido@google.com>
2011-12-15 14:20:06 -0800

    Update __version__ and RELEASE_NOTES for 0.9.4.

--- ndb/__init__.py
+++ ndb/__init__.py
@@ -1,10 +1,8 @@
 """NDB -- A new datastore API for the Google App Engine Python runtime."""
 
-__version__ = '0.9.3+'
+__version__ = '0.9.4'
 
 __all__ = []
-
-# TODO: Anything from eventloop or utils?  (They'd go here.)
 
 from tasklets import *
 __all__ += tasklets.__all__
