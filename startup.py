d3961a09ad07cd8d3ea41e21d2979810811b0cf4
Guido van Rossum <guido@google.com>
2012-01-24 21:59:08 -0800

    Register blobstore stub in startup.py.

--- startup.py
+++ startup.py
@@ -6,6 +6,8 @@ utils.tweak_logging()
 import os
 
 from google.appengine.api import apiproxy_stub_map
+from google.appengine.api.blobstore import dict_blob_storage
+from google.appengine.api.blobstore import blobstore_stub
 from google.appengine.api import datastore_file_stub
 from google.appengine.api import memcache
 from google.appengine.api.memcache import memcache_stub
@@ -25,6 +27,9 @@ tq_stub = taskqueue_stub.TaskQueueServiceStub()
 apiproxy_stub_map.apiproxy.RegisterStub('taskqueue', tq_stub)
 uf_stub = urlfetch_stub.URLFetchServiceStub()
 apiproxy_stub_map.apiproxy.RegisterStub('urlfetch', uf_stub)
+bs_storage = dict_blob_storage.DictBlobStorage()
+bs_stub = blobstore_stub.BlobstoreServiceStub(bs_storage)
+apiproxy_stub_map.apiproxy.RegisterStub('blobstore', bs_stub)
 os.environ['APPLICATION_ID'] = '_'
 
 class Employee(Model):
