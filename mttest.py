4fe1fade4d161ee55890e423fc6172c2fc787bcc
Guido van Rossum <guido@google.com>
2012-01-31 19:26:08 -0800

    Fix % formatting error.

---
+++ mttest.py
@@ -0,0 +1,45 @@
+"""A torture test to ferret out problems with multi-threading."""
+
+import sys
+import threading
+
+from ndb import tasklets
+from ndb import context
+from ndb import eventloop
+
+
+def main():
+  ##sys.stdout.write('_State.__bases__ = %r\n' % (eventloop._State.__bases__,))
+  num = 10
+  try:
+    num = int(sys.argv[1])
+  except Exception:
+    pass
+  threads = []
+  for i in range(num):
+    t = threading.Thread(target=one_thread, args=(i, num,))
+    t.start()
+    threads.append(t)
+  for t in threads:
+    t.join()
+
+
+@context.toplevel
+def one_thread(i, num):
+  ##sys.stdout.write('eventloop = 0x%x\n' % id(eventloop.get_event_loop()))
+  x = yield fibonacci(num)
+  sys.stdout.write('%d: %d --> %d\n' % (i, num, x))
+
+
+@tasklets.tasklet
+def fibonacci(n):
+  """A recursive Fibonacci to exercise task switching."""
+  if n <= 1:
+    raise tasklets.Return(n)
+  a = yield fibonacci(n - 1)
+  b = yield fibonacci(n - 2)
+  raise tasklets.Return(a + b)
+
+
+if __name__ == '__main__':
+  main()
---
+++ runtests.py
@@ -0,0 +1,39 @@
+"""Run all unittests."""
+
+__author__ = 'Beech Horn'
+
+import sys
+import unittest
+
+
+def suite():
+  mods = ['context', 'eventloop', 'key', 'metadata', 'model', 'polymodel',
+          'prospective_search', 'query', 'stats', 'tasklets', 'blobstore']
+  test_mods = ['%s_test' % name for name in mods]
+  ndb = __import__('ndb', fromlist=test_mods, level=1)
+
+  loader = unittest.TestLoader()
+  suite = unittest.TestSuite()
+
+  for mod in [getattr(ndb, name) for name in test_mods]:
+    for name in set(dir(mod)):
+      if name.endswith('Tests'):
+        test_module = getattr(mod, name)
+        tests = loader.loadTestsFromTestCase(test_module)
+        suite.addTests(tests)
+
+  return suite
+
+
+def main():
+  v = 1
+  for arg in sys.argv[1:]:
+    if arg.startswith('-v'):
+      v += arg.count('v')
+    elif arg == '-q':
+      v = 0
+  unittest.TextTestRunner(verbosity=v).run(suite())
+
+
+if __name__ == '__main__':
+  main()
