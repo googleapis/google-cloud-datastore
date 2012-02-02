3477f4b8fb8679afd722d2e5a8f443c89933c99a
Guido van Rossum <guido@google.com>
2012-02-01 19:28:13 -0800

    Merge in revamped GQL support. Fixes issue 114.

---
+++ demo/hello.py
@@ -0,0 +1,24 @@
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+from ndb import model, context
+
+class Greeting(model.Model):
+    message = model.StringProperty()
+    userid = model.IntegerProperty()  # Not used here, but later
+
+class HomePage(webapp.RequestHandler):
+    def get(self):
+        msg = Greeting.get_or_insert('hello', message='Hello world')
+        self.response.out.write(msg.message)
+
+urls = [('/.*', HomePage)]
+app = context.toplevel(webapp.WSGIApplication(urls).__call__)
+
+def main():
+    util.run_wsgi_app(app)
+
+if __name__ == '__main__':
+    main()
