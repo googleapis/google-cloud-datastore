1ad24fc86e539e9a0b32af29c24a4f0092cac736
Guillaume Binet <gbin@google.com>
2015-09-17 09:06:11 -0700

    Added classifiers to setup.py
    It helps people discovering it is python 2 only before trying it.

--- python/setup.py
+++ python/setup.py
@@ -28,5 +28,13 @@ setup(
     packages=['googledatastore'],
     package_dir={'googledatastore': 'googledatastore'},
     install_requires=['protobuf==2.5.0', 'oauth2client==1.2',
-                      'uritemplate==0.6']
+                      'uritemplate==0.6'],
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Topic :: Database",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+        ],
 )
